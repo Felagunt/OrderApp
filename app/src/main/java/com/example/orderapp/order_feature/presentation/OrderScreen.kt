650c3ab3eecfc3e3d5f019c9070a3a880ac3f0c8
Ilya <felagunt@live.ru>
2023-02-11 18:13:38 +0300

    staff

---
+++ app/src/main/java/com/example/orderapp/order_feature/presentation/OrderScreen.kt
@@ -0,0 +1,11 @@
+package com.example.orderapp.order_feature.presentation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun OrderScreen(
+    navController: NavController
+) {
+
+}
